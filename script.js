870d98f6b72f8b4b0b25ec121887a45179201d6e
Gariqz <ghassanariq7@gmail.com>
2025-02-28 17:28:01 +0700

    Changelog

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const messages = [
-    "Are you sure?",
-    "Really sure??",
-    "Are you positive?",
-    "Pookie please...",
-    "Just think about it!",
-    "If you say no, I will be really sad...",
-    "I will be very sad...",
-    "I will be very very very sad...",
-    "Ok fine, I will stop asking...",
-    "Just kidding, say yes please! ❤️"
+    "Bener ni?",
+    "Yang benerr??",
+    "Bener bangett?",
+    "Rill kah dek??",
+    "Berak enak lohh...",
+    "Berak lah bentar joo...",
+    "Berak woooo.........",
+    "AKU SUKA BERAKK!!!!",
+    "BERAKKK DAHHHH...",
+    "Berak kalo kamu sayang aku! ❤️"
 ];
 
 let messageIndex = 0;
